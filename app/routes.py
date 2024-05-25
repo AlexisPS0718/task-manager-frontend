d9ae39a760110ed63ea09a9556f4e1bef879413a
Alexis Peñuñuri Salgado <alxpesa@gmail.com>
2024-05-24 19:45:57 -0700

    Added new functions for creating and deleting tasks. New templates finished

--- app/routes.py
+++ app/routes.py
@@ -27,12 +27,30 @@ def task_list():
     response.status_code
   )
 
+@app.get("/tasks/new")
+def new_task():
+  return render_template("new.html")
+
+@app.post("/tasks/new")
+def post_task():
+  task_data = flask_req.form
+  response = requests.post(BACKEND_URL, json=task_data)
+  if response.status_code == 204:
+    return render_template("success.html", msg="Task created successfully")
+  return (
+    render_template("error.html", err_code=response.status_code),
+    response.status_code
+  )
+
 @app.get("/tasks/<int:pk>")
 def task_detail(pk):
   url = "%s/%s" % (BACKEND_URL, pk)
   response = requests.get(url)
   if response.status_code == 200:
     single_task = response.json().get("task")
+    if single_task == {}:
+      return (
+        render_template("error.html", err_code=404), 404)
     return render_template("detail.html", task=single_task)
   return (
     render_template("error.html", err_code=response.status_code),
@@ -44,7 +62,10 @@ def edit_task(pk):
   url = "%s/%s" % (BACKEND_URL, pk)
   response = requests.get(url)
   if response.status_code == 200:
-    single_task = response.josn().get("task")
+    single_task = response.json().get("task")
+    if single_task == {}:
+      return (
+        render_template("error.html", err_code=404), 404)
     return render_template("edit.html", task=single_task)
   return (
     render_template("error.html", err_code=response.status_code),
@@ -62,3 +83,38 @@ def edit_task_request(pk):
     render_template("error.html", err_code=response.status_code),
     response.status_code
   )
+
+@app.get("/tasks/<int:pk>/delete")
+def delete_task(pk):
+  url = "%s/%s" % (BACKEND_URL, pk)
+  response = requests.get(url)
+  if response.status_code == 200:
+    single_task = response.json().get("task")
+    if single_task == {}:
+      return (
+        render_template("error.html", err_code=404), 404)
+    return render_template("delete.html", task=single_task)
+  return (
+    render_template("error.html", err_code=response.status_code),
+    response.status_code
+  )
+
+@app.post("/tasks/<int:pk>/delete")
+def delete_task_request(pk):
+  url = "%s/%s" % (BACKEND_URL, pk)
+  response = requests.delete(url)
+  if response.status_code == 204:
+    return render_template("success.html", msg="Task deleted successfully")
+  return (
+    render_template("error.html", err_code=response.status_code),
+    response.status_code
+  )
+
+# For testing purposes
+@app.get("/error")
+def error():
+  return render_template("error.html")
+
+@app.get("/success")
+def success():
+  return render_template("success.html")
